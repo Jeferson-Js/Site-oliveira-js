aaa1b87bf0328fba8278c524d08a73ad24780137
Jeferson Oliveira <82729145+Jeferson-Js@users.noreply.github.com>
2022-04-02 19:25:10 -0300

    conectando a pagian com o banco de dados

---
+++ conexao.js
@@ -0,0 +1,20 @@
+/*const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+host: 'localhost',
+user: 'root',
+database: 'formulario-js',
+password: ''
+});
+
+connection.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log("Conexao realizada com sucesso");
+    }
+});
+
+connection.end();*/
+
+//Como fazer a coneção do banco de dados com mysql em node.js
